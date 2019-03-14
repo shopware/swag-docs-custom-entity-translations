39d8e7357477efd574f1dfafd0f3dc8b209f8074
Patrick Stahl <p.stahl@shopware.com>
2019-03-14 15:04:19 +0100

    Add class php doc

--- Custom/Aggregate/CustomTranslation/CustomEntityTranslationCollection.php
+++ Custom/Aggregate/CustomTranslation/CustomEntityTranslationCollection.php
@@ -4,6 +4,15 @@ namespace CustomEntityTranslations\Custom\Aggregate\CustomTranslation;
 
 use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
 
+/**
+ * @method void                         add(CustomTranslationEntity $entity)
+ * @method void                         set(string $key, CustomTranslationEntity $entity)
+ * @method CustomTranslationEntity[]    getIterator()
+ * @method CustomTranslationEntity[]    getElements()
+ * @method CustomTranslationEntity|null get(string $key)
+ * @method CustomTranslationEntity|null first()
+ * @method CustomTranslationEntity|null last()
+ */
 class CustomEntityTranslationCollection extends EntityCollection
 {
     public function getCustomEntityIds(): array
