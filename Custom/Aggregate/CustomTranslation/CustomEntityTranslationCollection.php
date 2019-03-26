7d8d265363c0a034097b3765d43fb3a459784ec3
Patrick Stahl <p.stahl@shopware.com>
2019-03-26 16:07:04 +0100

    Remove unnecessary helper methods

--- Custom/Aggregate/CustomTranslation/CustomEntityTranslationCollection.php
+++ Custom/Aggregate/CustomTranslation/CustomEntityTranslationCollection.php
@@ -15,27 +15,6 @@ use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
  */
 class CustomEntityTranslationCollection extends EntityCollection
 {
-    public function getCustomEntityIds(): array
-    {
-        return $this->fmap(function (CustomTranslationEntity $customTranslationEntity) {
-            return $customTranslationEntity->getCustomEntityId();
-        });
-    }
-
-    public function filterByCustomEntityId(string $id): self
-    {
-        return $this->filter(function (CustomTranslationEntity $customTranslationEntity) use ($id) {
-            return $customTranslationEntity->getCustomEntityId() === $id;
-        });
-    }
-
-    public function getLanguageIds(): array
-    {
-        return $this->fmap(function (CustomTranslationEntity $customTranslationEntity) {
-            return $customTranslationEntity->getLanguageId();
-        });
-    }
-
     public function filterByLanguageId(string $id): self
     {
         return $this->filter(function (CustomTranslationEntity $customTranslationEntity) use ($id) {
